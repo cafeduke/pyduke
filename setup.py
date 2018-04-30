86a126d27452ce85cb877d80ac00f1b964ffc634
Raghu Nandan <raghubs81@gmail.com>
2018-04-30 15:24:01 +0530

    Version 1.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyduke',
-    version='1.3',
+    version='1.3.1',
     description='Duke python utilities',
     url='http://github.com/cafeduke/pyduke',
     author='Raghunandan Seshadri',
